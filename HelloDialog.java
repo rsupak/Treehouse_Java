e7d3c4b277a4cb5a17afadb7c6f7c8c3ff380b55
Richard Supak <richard.supak@icloud.com>
2018-08-13 19:01:01 -0500

    added new dialog file

---
+++ HelloDialog.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class HelloDialog
+{
+    public static void main(String[] args)
+    {
+        javax.swing.JOptionPane.showMessageDialog(null, "Hello, World!");
+    }
+}
